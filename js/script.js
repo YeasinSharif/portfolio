294928c248d0651a8588fbb655f9a61523305944
YeasinSharif <mahfujakash2@gmail.com>
2022-03-08 15:11:12 +0600

    commit first

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    /* slider */
+
+    $('.slider-wrapper').slick({
+        prevArrow: '<i class="arrow1 far fa-arrow-left"></i>',
+        nextArrow: '<i class="arrow2 far fa-arrow-right"></i>',
+        
+        fade: false,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        speed: 1000 
+    })
+
+
+})
