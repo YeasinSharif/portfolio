eab26921809207b81730a19f5a2376fa58f55a56
YeasinSharif <mahfujakash2@gmail.com>
2022-03-09 12:46:39 +0600

    commit to

--- js/script.js
+++ js/script.js
@@ -13,4 +13,67 @@ $(function () {
     })
 
 
+
+
+
+
+
+
+/* typed js starts */
+
+const typed = new Typed('.test-type', {
+  
+  strings: ['Yeasin Sharif',
+
+  
+            
+        ],
+        
+        backSpeed: 100,
+        typeSpeed: 100,
+        loop: true,
+
+});
+
+/* typed js ends */
+
+/* scroll top button js starts */
+
+    $(window).on('scroll', function() {
+        
+        if($(this).scrollTop() > 450 ) {
+            $('#topbtn').fadeIn(500);
+        }else{
+            $('#topbtn').fadeOut(500);
+        }
+
+    })
+
+
+    
+    $('#topbtn').on('click', function () {
+        $('html,body').animate({
+            scrollTop: 0,
+        },1000)
+    })
+
+    /* scroll top button js ends */
+
+    /* smooth jump js starts*/ 
+
+    $('a').on('click', function(event){
+        event.preventDefault;
+        if(this.hash !== '') {
+            $('html,body').animate({
+                scrollTop:$(this.hash).offset().top,
+            },1000)
+        }
+    })
+
+    /* smooth jump js ends*/
+
+
+
 })
+
+
